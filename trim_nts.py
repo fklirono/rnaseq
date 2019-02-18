ec5fe8a5b7359de4bbc6c05f14452a2007921038
Filippos Klironomos <fklirono@gmx.de>
2019-02-18 13:17:02 +0100

    simplified trim_nts.py to accept only from STDIN and output only to STDOUT

--- trim_nts.py
+++ trim_nts.py
@@ -3,12 +3,8 @@
 ###################################################################################################
 #  Filippos Klironomos, Berlin Institute for Medical Systems Biology, Max-Delbrück Center, Berlin
 ###################################################################################################
-#
-#  this code trims -5 n5p -3 n3p number of nucleotides from the 5' and 3' end of reads respectively
-#
-###################################################################################################
 usage = """
-  %prog [--min 0] n5p n3p [reads.fastq|-] [trimmed.fastq|-]
+  cat reads.fastq | %prog [--min 0] n5p n3p > trimmed.fastq
 """
 
 from Bio import SeqIO
@@ -18,25 +14,15 @@ import sys
 parser=ArgumentParser(description=usage, formatter_class=RawDescriptionHelpFormatter)
 parser.add_argument('n5p', default=0, type=int, nargs='?', action='store', help="number of nucleotides to trim from the 5' end (default 0)")
 parser.add_argument('n3p', default=0, type=int, nargs='?', action='store', help="number of nucleotides to trim from the 3' end (default 0)")
-parser.add_argument('reads', default='', type=str, nargs='?', action='store', help='FASTQ file of input sequences or STDIN')
-parser.add_argument('trimmed', default='' , type=str, nargs='?', action='store', help='FASTQ file of trimmed sequences or STDOUT')
 parser.add_argument('--min', default=0, type=int, nargs='?', action='store', help='filter our after trimming reads of length less than this')
 parser.add_argument('--nospace', default=False, action='store_true', dest='nospace', help='rename reads by keeping the readID up to first space character?')
 options = parser.parse_args()
+OUT=sys.stdout
+IN=sys.stdin
 
-if (options.trimmed in ('','-')):
-  OUT=sys.stdout
-else:
-  OUT=options.trimmed
-
-if (options.reads in ('', '-')):
-  IN=sys.stdin
-else:
-  IN=options.reads
 
 #import ipdb;  ipdb.set_trace()
 
-#sys.stderr.write('\nprocessing the reads...')
 
 for r in SeqIO.parse(IN, 'fastq'):
   l=len(r)
@@ -46,4 +32,3 @@ for r in SeqIO.parse(IN, 'fastq'):
     r.description=r.id
   SeqIO.write(r[options.n5p:(l-options.n3p)], OUT, 'fastq')
 
-#sys.stderr.write('done!\n')
